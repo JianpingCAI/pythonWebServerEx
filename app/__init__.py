c92c40882c9c95ea0b43203b24558db9812a6721
JianpingCai <cai-jianping@outlook.com>
2023-04-21 16:29:55 +0800

    add logging and monitoring (Sentry SDK)

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
+import logging
+from logging.handlers import RotatingFileHandler
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -13,5 +17,34 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Initialize Sentry
+if Config.SENTRY_DSN:
+    sentry_sdk.init(
+        dsn=Config.SENTRY_DSN,
+        integrations=[FlaskIntegration()],
+        environment='production',
+    )
+
+# Set up logging
+if not app.debug:
+    # Set up logging to a file
+    log_level = logging.getLevelName(Config.LOG_LEVEL)
+    file_handler = RotatingFileHandler('regression_testing_system.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    file_handler.setLevel(log_level)
+    app.logger.addHandler(file_handler)
+
+    # Set up logging to console
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
+    console_handler.setLevel(log_level)
+    app.logger.addHandler(console_handler)
+
+    app.logger.setLevel(log_level)
+    app.logger.info('Regression testing system startup')
+    
 # Import the views module to register the routes
 from app import views
+
--- config.py
+++ config.py
@@ -3,7 +3,14 @@ import os
 
 
 class Config(object):
+    # SQL Alchemy configuration
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get("DATABASE_URL") or "sqlite:///regression_testing_system.db"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Logging configuration
+    LOG_LEVEL = os.environ.get('LOG_LEVEL') or 'INFO'
+
+    # Sentry configuration
+    SENTRY_DSN = os.environ.get('SENTRY_DSN')
